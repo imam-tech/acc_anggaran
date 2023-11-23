b8a0a6d109ba044979af5456ad6f2184d470a4c6
imam syafii <imam@importir.com>
2023-11-23 16:27:31 +0700

    add

--- app/Repositories/CoaRepository.php
+++ app/Repositories/CoaRepository.php
@@ -19,7 +19,6 @@ class CoaRepository {
                 "account_name" => "required",
                 "account_type" => "required",
             ]);
-            return resultFunction("", false, $companyId);
 
             if ($validator->fails()) return resultFunction("Err code CR-S: " . collect($validator->errors()->all())->implode(" , "));
 
