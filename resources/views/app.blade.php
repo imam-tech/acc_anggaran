fc3bb0571edc04d11691c4ac79688fa4dd55e8db
imam syafii <imam@importir.com>
2023-11-26 15:39:06 +0700

    add

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>FAT System</title>
+    <title>FAT System (Finance Accounting Tax System)</title>
     <link rel="stylesheet" href="{{ asset(mix('css/main.css')) }}">
     <link rel="stylesheet" href="{{ asset(mix('css/app.css')) }}">
 
