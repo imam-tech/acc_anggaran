1d4a14f23410896ca1f15232ae76bab282f9f7d7
imam syafii <imam@importir.com>
2023-11-26 15:37:21 +0700

    add

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Anggaran & Accounting</title>
+    <title>FAT System</title>
     <link rel="stylesheet" href="{{ asset(mix('css/main.css')) }}">
     <link rel="stylesheet" href="{{ asset(mix('css/app.css')) }}">
 
